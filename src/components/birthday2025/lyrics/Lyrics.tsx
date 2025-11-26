849d61ff5534a7c3e0f462a9ae8c652ae65102f6
mrwsantos <will.wsa@hotmail.com>
2025-11-26 13:33:47 +1300

    birthday 2025

--- src/components/birthday2025/lyrics/Lyrics.tsx
+++ src/components/birthday2025/lyrics/Lyrics.tsx
@@ -23,7 +23,7 @@ const Lyrics: React.FC<LyricsProps> = ({ setLyricsOpened }) => {
       >
         <b>Verse 1:</b>
         <p>The moment I saw your face for the first time</p>
-        <p>I couldn't imagine I'd wanna be with you all my life</p>
+        <p>{"I couldn't imagine I'd wanna be with you all my life"}</p>
 
         <b>Chorus 1:</b>
         <p>Something happened since then</p>
@@ -31,26 +31,26 @@ const Lyrics: React.FC<LyricsProps> = ({ setLyricsOpened }) => {
         <p>That I cannot explain</p>
 
         <b>Verse 2:</b>
-        <p>You're the kind of girl that I thought I'd never find</p>
+        <p>{"You're the kind of girl that I thought I'd never find"}</p>
         <p>Not even this long distance could break us apart</p>
 
         <b>Chorus 2:</b>
-        <p>I've been waiting for you</p>
+        <p>{"I've been waiting for you"}</p>
         <p>My dark skies turned into blue</p>
         <p>And I cannot explain</p>
 
         <p style={{ margin: "20px 0" }}>🎵🎵🎵</p>
 
         <b>Bridge 1:</b>
-        <p>I'll never gonna leave your side</p>
-        <p>You'll never gonna be alone</p>
+        <p>{"I'll never gonna leave your side"}</p>
+        <p>{"You'll never gonna be alone"}</p>
         <p>This love is within our bones</p>
         <p>I see myself in your eyes</p>
 
         <b>Bridge 2:</b>
         <p>I can see your smiling face</p>
         <p>I want you to feel amazed</p>
-        <p>I'm in every part of you</p>
+        <p>{"I'm in every part of you"}</p>
         <p>愛に心を賭ける</p>
 
         <b>Final:</b>
